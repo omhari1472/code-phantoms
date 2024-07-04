bf2a92b6a9d571ed87ee99451bce5d3b30d15d95
Hari Om <omhari1472@gmail.com>
2024-07-04 21:55:48 +0530

    express setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'; 
+import mongoose from 'mongoose';
+
+const app = express()
+app.use(cors()); 
+app.use(express.json());  
+
+// mongoose.connect("mongodb+srv://omhari1472:<password>@cluster0.f1fccls.mongodb.net/")
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke 💩')
+})
+
+app.listen(4000, () => {
+  console.log('Server is running on port 4000')
+})
