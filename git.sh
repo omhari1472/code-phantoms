e1fafd096a0f7cb9270c5a3fb82b5ac34b55c201
Your Name <118709199+kishorcha@users.noreply.github.com>
2024-07-04 23:36:48 +0530

    project setup in kishor laptop

--- git.sh
+++ git.sh
@@ -1,48 +1,45 @@
-#!/bin/bash
+  #!/bin/bash
+  echo "
+  _    _ _   _ ____  _    _ _   _
+  / \\  / \\_/  /  _ \\ / \\  / \\_/ \\
+  \\_/  \\_/ \\_/\\_/ \\_/ \\_/  \\_/ \\_/
+  "
+  echo "Running Git Commands..."
+  if [ -z "$1" ]; then
+    echo "Error: Please provide a commit message"
+    exit 1
+  fi
 
-echo "
- _    _ _   _ ____  _    _ _   _
-/ \\  / \\_/  /  _ \\ / \\  / \\_/ \\
-\\_/  \\_/ \\_/\\_/ \\_/ \\_/  \\_/ \\_/
-"
-echo "Running Git Commands..."
+  commit="$1"
 
-
-if [ -z "$1" ]; then
-  echo "Error: Please provide a commit message"
-  exit 1
-fi
-
-commit="$1"
-
-git add .
+  git add .
 
 
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to add files to the staging area."
-  exit 1
-fi
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to add files to the staging area."
+    exit 1
+  fi
 
-sleep 2
+  sleep 2
 
-git status
+  git status
 
-message="$1"  
+  message="$1"  
 
-git commit -m "$message"
+  git commit -m "$message"
 
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to commit changes."
-  exit 1
-fi
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to commit changes."
+    exit 1
+  fi
 
-branch_name="main"
+  branch_name="main"
 
-git push origin "$branch_name"
+  git push origin "$branch_name"
 
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to push changes to the remote repository."
-  exit 1
-fi
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to push changes to the remote repository."
+    exit 1
+  fi
 
-echo "Git commands executed successfully."
+  echo "Git commands executed successfully."
